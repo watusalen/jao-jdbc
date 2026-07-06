af593cc7560b5a85f4c236b9e1433a15ad0a1739
Matusalen Alves <matusalencosta@icloud.com>
2026-07-06 15:05:49 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,23 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department dp1 = new Department(1, "Digital");
-        System.out.println(dp1.hashCode());
+        Department department = new Department(1, "Digital");
+        Seller seller = new Seller(
+                1,
+                "Bob",
+                "bob@gmail.com",
+                new Date(),
+                3000.0,
+                department
+        );
+
+        System.out.println(seller);
+
     }
 }

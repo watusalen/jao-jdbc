d81082fbd9db80d8bd2a0ca98ace6c1f44b93703
Matusalen Alves <matusalencosta@icloud.com>
2026-07-07 09:27:12 -0300

    createDepartmentDAO implementation in DAOFactory

--- src/model/dao/factory/DAOFactory.java
+++ src/model/dao/factory/DAOFactory.java
@@ -1,11 +1,17 @@
 package model.dao.factory;
 
 import db.DataBase;
+import model.dao.DepartmentDAO;
 import model.dao.SellerDAO;
+import model.dao.implementation.DepartmentJDBC;
 import model.dao.implementation.SellerJDBC;
 
 public class DAOFactory {
     public static SellerDAO createSellerDAO() {
         return new SellerJDBC(DataBase.getConnection());
+    }
+
+    public static DepartmentDAO createDepartmentDAO() {
+        return new DepartmentJDBC(DataBase.getConnection());
     }
 }
